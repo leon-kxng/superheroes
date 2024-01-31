600b7367d2db1f70e32f8406545c1e57b210800c
Leon <leon.kigen@student.moringaschool.com>
2024-01-31 13:15:32 +0300

    Refactor codebase to use new database schema.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 import os
 
@@ -9,9 +9,9 @@ from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
 # Define the path to the database file
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 
 
@@ -20,6 +20,10 @@ db.init_app(app)
 
 # Routes
 
+@app.route('/')
+def home():
+    return jsonify({'message':'superheroes rule'})
+
 @app.route('/heroes', methods=['GET'])
 def get_heroes():
     heroes = Hero.query.all()
@@ -27,15 +31,21 @@ def get_heroes():
 
 @app.route('/heroes/<int:hero_id>', methods=['GET'])
 def get_hero(hero_id):
-    hero = Hero.query.get(hero_id)
+    hero = Hero.query.filter(Hero.id == hero_id).first()
 
     if hero:
-        return jsonify({
+        hero_dict = {
             'id': hero.id,
             'name': hero.name,
             'super_name': hero.super_name,
-            'powers': [{'id': power.id, 'name': power.name, 'description': power.description} for power in hero.powers]
-        })
+            'powers': [{'id': hero_power.power.id, 'name': hero_power.power.name, 'description': hero_power.power.description} for hero_power in hero.hero_power]
+        }
+        response = make_response(
+            jsonify(hero_dict),
+            200,
+        )
+
+        return response
     else:
         return jsonify({'error': 'Hero not found'}), 404
 
