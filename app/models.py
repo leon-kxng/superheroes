6ccf5bc85ceb49922cc211abb32d20a37e5dfe0f
Leon <leon.kigen@student.moringaschool.com>
2024-01-31 13:15:42 +0300

    Refactor codebase to use new database schema.

--- app/models.py
+++ app/models.py
@@ -3,38 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Hero(db.Model):
-    __tablename__ = 'hero'
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     super_name = db.Column(db.String(50), nullable=False)
 
     # Relationship with HeroPower
-    hero_powers = db.relationship('HeroPower', back_populates='hero')
-
-    # Relationship with Power through HeroPower
-    powers = db.relationship('Power', secondary='hero_power', back_populates='heroes')
+    hero_power = db.relationship('HeroPower', backref='hero')
 
 class Power(db.Model):
-    __tablename__ = 'power'
+    __tablename__ = 'powers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(255), nullable=False)
 
     # Relationship with Hero through HeroPower
-    heroes = db.relationship('Hero', secondary='hero_power', back_populates='powers')
+    power_heroes = db.relationship('HeroPower', backref='power')
 
 class HeroPower(db.Model):
-    __tablename__ = 'hero_power'
+    __tablename__ = 'hero_powers'
 
     id = db.Column(db.Integer, primary_key=True)
-    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
-    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
     strength = db.Column(db.String(20), nullable=False)
 
-    # Relationship with Hero
-    hero = db.relationship('Hero', back_populates='hero_powers')
-
-    # Relationship with Power
-    power = db.relationship('Power', back_populates='heroes')
+   
